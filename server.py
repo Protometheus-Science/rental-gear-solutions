507f26e1a5af9a1ff0d981bba00794367962da32
jacksarick <jacksarick@gmail.com>
2017-07-05 00:06:25 -0600

    Sample tests work fine. Will implement authentication next

--- server.py
+++ server.py
@@ -19,6 +19,14 @@ def hello():
 def login():
 	return render_template('login.html', name=request.args.get('name', ''))
 
+@app.route("/auth", methods=['POST', 'GET'])
+def authenticate():
+	error = "you the system took a wrong turn somewhere"
+	if request.method == 'POST':
+		error = "your password is " + request.form['password']
+
+	return render_template('login.html', error=error, name=request.args.get('name', ''))
+
 # Start App
 app.run(host='127.0.0.1', port=5000, 
     debug = True, ssl_context=context)
