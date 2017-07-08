68cba1abc37bb32640c87371130fa810dd6cbd89
jacksarick <jacksarick@gmail.com>
2017-07-08 14:04:42 -0600

    I'm an idiot, the filtering is totally broken I'll fix it later

--- static/js/inventory.js
+++ static/js/inventory.js
@@ -7,3 +7,14 @@ function toggle_filters() {
 		$('#filters').hide();
 	}
 }
+
+function update_filters() {
+	filters = [...document.getElementsByClassName("filter-parameter")].map(x => x.value)
+	items = [...document.getElementsByClassName("inventory-item")]
+	for (var i = items.length - 1; i >= 0; i--) {
+		items[i].style.display = "none";
+		if (filters.includes(items[i])) {
+			items[i].style.display = "table-row";
+		}
+	}
+}
