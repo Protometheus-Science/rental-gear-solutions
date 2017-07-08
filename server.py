e1e1f6b66758270de8323cf52d2488e81b8ff9d9
jacksarick <jacksarick@gmail.com>
2017-07-08 00:08:40 -0600

    Added main page in

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_compress import Compress
-from modules import authenticate
+from modules import authenticate, read_data
 import ssl
 
 # Generate app
@@ -29,6 +29,14 @@ def login():
 	# Let them know they failed if the password was wrong
 	return render_template('login.html', error="your password was incorrect", name=request.args.get('name', ''))
 
+@app.route("/main")
+def main_page():
+	# if the user authenticates...
+	if True:
+		r = read_data
+		csv = r.csv('./data/stock_1.csv')
+		print r.parse(csv)
+		return render_template('main.html', columns=r.columns(csv), items=r.parse(csv))
 
 # Start App
 app.run(host='127.0.0.1', port=5000, 
