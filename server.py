0a819914ec51140f0331262f0cc1e68b926fd3aa
jacksarick <jacksarick@gmail.com>
2017-07-08 00:24:28 -0600

    Line length was bothering me

--- server.py
+++ server.py
@@ -20,14 +20,17 @@ def hello():
 def login():
 	# if it is a user visiting...
 	if request.method == 'GET':
-		return render_template('login.html', name=request.args.get('name', ''))
+		return render_template('login.html',
+			name=request.args.get('name', ''))
 	
 	# Else check the password
 	if authenticate.password(request.form['password']):
-		return render_template('login.html', error="you were logged in succesfully")
+		return redirect("/main")
 	
 	# Let them know they failed if the password was wrong
-	return render_template('login.html', error="your password was incorrect", name=request.args.get('name', ''))
+	return render_template('login.html',
+		error="your password was incorrect",
+		name=request.args.get('name', ''))
 
 @app.route("/main")
 def main_page():
@@ -36,7 +39,9 @@ def main_page():
 		r = read_data
 		csv = r.csv('./data/stock_1.csv')
 		print r.parse(csv)
-		return render_template('main.html', columns=r.columns(csv), items=r.parse(csv))
+		return render_template('main.html',
+			columns=r.columns(csv),
+			items=r.parse(csv))
 
 # Start App
 app.run(host='127.0.0.1', port=5000, 
