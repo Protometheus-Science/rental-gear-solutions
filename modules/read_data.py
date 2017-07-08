64240dd9fa240bdb85510f4b128f464fbe1edf6b
jacksarick <jacksarick@gmail.com>
2017-07-08 13:44:10 -0600

    Updated how columns get read

--- modules/read_data.py
+++ modules/read_data.py
@@ -3,11 +3,11 @@ def csv(path):
 	return [line.strip().split(",") for line in open(path)]
 
 # Pull column from csv_data
-def columns(csv_data):
-	return map(lambda x: x.title(), csv_data[0])
+def columns(csv):
+	# rotate matrix 90 cc: zip(*original)[::-1]
+	return dict([(line[0], set(line[1:])) for line in zip(*csv)[::-1]])
 
 # Return data in jinja-usable format
 def parse(csv):
-	data = map(dict, [[("count", csv.count(item))] + zip(columns(csv), item) for item in csv[1:]])
-	print data
+	data = map(dict, [[("count", csv.count(item))] + zip(csv[0], item) for item in csv[1:]])
 	return data
