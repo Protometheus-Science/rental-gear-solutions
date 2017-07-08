5ca0dd8c81dcc577ef4d7004803f826bc216ce2b
jacksarick <jacksarick@gmail.com>
2017-07-08 00:08:55 -0600

    Can now parse csv into useful data

--- modules/read_data.py
+++ modules/read_data.py
@@ -1,2 +1,13 @@
+# Read a csv file into a 2d array
 def csv(path):
-	return [line.split(",") for line in open("data/stock_1.csv")]
+	return [line.strip().split(",") for line in open(path)]
+
+# Pull column from csv_data
+def columns(csv_data):
+	return map(lambda x: x.title(), csv_data[0])
+
+# Return data in jinja-usable format
+def parse(csv):
+	data = map(dict, [[("count", csv.count(item))] + zip(columns(csv), item) for item in csv[1:]])
+	print data
+	return data
