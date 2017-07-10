752224826c3f819bb4cfa216a7690578deb9c007
jacksarick <jacksarick@gmail.com>
2017-07-09 22:34:20 -0600

    Fine tuned parameters

--- data/generate_random_data.py
+++ data/generate_random_data.py
@@ -9,7 +9,7 @@ pick = lambda l: l[randint(0, len(l)-1)]
 
 def new_bike():
 	bike = ["bike", pick(store), pick(brand), pick(style)]
-	bike += [bike[3] + "-" + str(randint(1, 2)), pick(size)]
+	bike += [bike[2] + "-" + bike[3] + "-" + str(randint(1, 2)), pick(size)]
 	return bike
 
-print "\n".join(map(lambda x: ",".join(x), [new_bike() for i in range(100)]))
+print "\n".join(map(lambda x: ",".join(x), [new_bike() for i in range(500)]))
