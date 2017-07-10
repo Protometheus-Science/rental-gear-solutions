e44beadc7c67b8cfd8c363d796cd6718fe1ba997
jacksarick <jacksarick@gmail.com>
2017-07-09 21:47:38 -0600

    filter function works now

--- static/js/inventory.js
+++ static/js/inventory.js
@@ -9,11 +9,23 @@ function toggle_filters() {
 }
 
 function update_filters() {
-	filters = [...document.getElementsByClassName("filter-parameter")].map(x => x.value)
+	// Pull all filters into array and get the values of checked ones
+	filters = [...document.getElementsByClassName("filter-parameter")].filter(x => x.checked).map(x => x.value)
+
+	// Pull all items
 	items = [...document.getElementsByClassName("inventory-item")]
+	
 	for (var i = items.length - 1; i >= 0; i--) {
+		// default to hidden
 		items[i].style.display = "none";
-		if (filters.includes(items[i])) {
+		
+		// Pull item ids
+		
+		let ids = [...items[i].childNodes].map(x => x.id);
+		// Find matches between ids and search paramets
+		let match = ids.map(x => filters.includes(x)).includes(true)
+
+		if (match) {
 			items[i].style.display = "table-row";
 		}
 	}
